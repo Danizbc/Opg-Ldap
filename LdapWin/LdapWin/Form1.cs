1cfcb0af3cfc1d309589c8234fde025363cb82bf
Daniel Sørensen <danielftw321@gmail.com>
2019-02-28 10:32:58 +0100

    Update

---
+++ LdapWin/LdapWin/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LdapWin
+{
+    public partial class Form1 : Form
+    {
+        Logic l = new Logic();
+
+        string firstname;
+
+        string lastname;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = textBox1;
+            textBox1.Focus();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (firstname != null && lastname != null)
+            {
+                firstname += (" " + lastname);
+                button1_Click(l.LdapConnect(firstname), e);
+
+                var items = listView1.Items;
+                foreach (var item in l.Information)
+                {
+                    items.Add(item);
+                }
+            }
+            else
+            {
+                
+                MessageBox.Show("Enter a name or last name");
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            firstname = textBox1.Text;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            lastname = textBox2.Text;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selecteditems = listView1.SelectedItems;
+            if (selecteditems.Count > 0)
+            {
+                this.Text = selecteditems[0].Text;
+            }
+            else
+            {
+                this.Text = "empty";
+            }
+        }
+    }
+}
---
+++ LdapWin/LdapWin/Logic.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Text;
+using System.DirectoryServices;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Collections.Generic;
+
+namespace LdapWin
+{
+    class Logic
+    {
+        //Attribute
+        bool trueOrFalse = true;
+
+        List<string> information = new List<string>();
+
+        readonly string Failed = "Failed";
+
+        readonly string working = "Working";
+
+        //Properties
+        public bool TrueOrFalse
+        {
+            get { return trueOrFalse; }
+            set
+            {
+                trueOrFalse = value;
+            }
+        }
+
+        public List<string> Information
+        {
+            get { return information; }
+            set
+            {
+                information = value;
+            }
+        }
+
+        public string LdapConnect(string Uname)
+        {
+
+            string username = Uname;
+
+            try
+            {
+                // create LDAP connection object  
+
+                //mLdapConnetion is our connection to AD if there is no result comming out
+                ///Check for the right connection string
+                DirectoryEntry myLdapConnection = createDirectoryEntry();
+
+                // create search object which operates on LDAP connection object  
+                // and set search object to only find the user specified  
+
+                ///Search filter becomes what we typed at the start                
+                DirectorySearcher search = new DirectorySearcher(myLdapConnection);
+                search.Filter = "(cn=" + username + ")";
+
+                // create results objects from search object  
+                SearchResult result = search.FindOne();
+
+                ///When result is found 
+                if (result != null)
+                {
+                    // user exists, cycle through LDAP fields (cn, telephonenumber etc.)  
+
+                    ResultPropertyCollection fields = result.Properties;
+
+                    ///Searches for all results that have pur input inside?
+                    foreach (string ldapField in fields.PropertyNames)
+                    {
+                        // cycle through objects in each field e.g. group membership  
+                        // (for many fields there will only be one object such as name)  
+
+                        foreach (object myCollection in fields[ldapField])
+                            information.Add(string.Format("{0,-20} : {1}",
+                                          ldapField, myCollection.ToString()));
+                    }
+                }
+                ///If it cant find the user
+                ///IT will not say this if connetion string is wrong
+                else
+                {
+                    // user does not exist  
+
+                    return Failed;
+                }
+            }
+
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+
+            return working;
+        }
+        /// <summary>
+        /// Connection for our AD
+        /// </summary>
+        static DirectoryEntry createDirectoryEntry()
+        {
+            // create and return new LDAP connection with desired settings  
+            ///We dont need a path? 
+            ///Try the one below if it doesnt work
+            /// DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://MMDA.DK"); 
+            DirectoryEntry ldapConnection = new DirectoryEntry("MILJØ.DK");
+            ///We only need a path if we want to be very specific in what OU we want to look for
+            ldapConnection.Path = "LDAP://OU=DomainUsers,DC=miljø,DC=dk"; ///We are searching for it in miljømærkering
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure; ///makes secure connetion?
+
+            return ldapConnection;
+            ///return directoryEntry
+        }
+
+
+
+
+    }
+
+
+
+}
+
